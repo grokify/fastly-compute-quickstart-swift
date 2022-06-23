c7cfcf4262187ba2286c9b57466def47f5a3652d
grokify <johncwang@gmail.com>
2022-06-23 14:35:12 -0700

    update logging info

--- Sources/FastlyComputeQuickstart/FastlyComputeQuickstart.swift
+++ Sources/FastlyComputeQuickstart/FastlyComputeQuickstart.swift
@@ -10,14 +10,14 @@ struct Response: Codable {
 struct ComputeApp {
     static func main() async throws {
         try await onIncomingRequest { req, res in
-            let logger = try Logger(name: "quickstartLogger")
+            let logger = try Logger(name: "QuickstartLog")
             switch (req.method, req.url.path) {
             case (.post, "/quickstart"):
                 let content = Response(foo: "bar", hello: "world", ping: "pong")
-                try logger.write("responded with 200")
+                try logger.write("swift quickstart responded with 200")
                 try await res.status(200).send(content)
             default:
-                try logger.write("responded with 404")
+                try logger.write("swift quickstart responded with 404")
                 try await res.status(404).send()
             }
         }
