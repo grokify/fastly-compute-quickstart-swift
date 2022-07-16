e54ec0aadf03b60d32f890f2089ef0807520b74f
John Wang <johncwang@gmail.com>
2022-07-15 21:56:31 -0700

    enhance: add HTML homepage

--- Sources/FastlyComputeQuickstart/FastlyComputeQuickstart.swift
+++ Sources/FastlyComputeQuickstart/FastlyComputeQuickstart.swift
@@ -9,21 +9,26 @@ struct Response: Codable {
 struct ComputeApp {
     static func main() async throws {
         try await onIncomingRequest { req, res in
-            let logger = try Logger(name: "QuickstartLog")
+            // let logger = try Logger(name: "QuickstartLog") // Logger is not yet supported.
             switch (req.method, req.url.path) {
+            case (.get, "/"):
+                try await res
+                    .header("Content-Type", "text/html; charset=utf-8")
+                    .status(200)
+                    .send("<!DOCTYPE html><html><body><h1>Hello, Swift World!</h1></body></html>")
             case (.post, "/quickstart"):
                 if let accept = req.headers.get("Accept") {
                     if accept.caseInsensitiveCompare("text/plain") == .orderedSame {
-                        try logger.write("swift quickstart responded with 200")
+                        // try logger.write("swift quickstart responded with 200")
                         try await res.status(200).send("Hello, Swift World!")
                         return
                     }
                 }
                 let content = Response(version: 1, message: "Hello, Swift World!")
-                try logger.write("swift quickstart responded with 200")
+                // try logger.write("swift quickstart responded with 200")
                 try await res.status(200).send(content)
             default:
-                try logger.write("swift quickstart responded with 404")
+                // try logger.write("swift quickstart responded with 404")
                 try await res.status(404).send()
             }
         }
